6d989c87bf8e8077130b7b90bab3f318591f5b0c
bk7191 <bk7191014@gmail.com>
2024-05-15 22:54:03 +0200

    serializers et viewsets, permissions authenticate !!  rest_framework.request.WrappedAttributeError: 'IsAuthenticated' object has no attribute 'authenticate'

--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,8 @@
-from django.contrib.auth.models import User
-from rest_framework import viewsets
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets, permissions
 from rest_framework.permissions import IsAuthenticated
 from .models import CustomUser
-from .serializers import CustomUserSerializer, UserSerializer
+from .serializers import CustomUserSerializer, UserSerializer, GroupSerializer
 from .permissions import IsOwnerOrReadOnly
 
 
@@ -17,5 +17,15 @@ class CustomUserViewSet(viewsets.ModelViewSet):
 
 # ViewSets define the view behavior.
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
+    queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all().order_by('name')
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
