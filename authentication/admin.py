9dc3d32bd439f259c034490572e0a8a69026f548
bk7191 <bk7191014@gmail.com>
2024-06-05 14:55:08 +0200

    isauthenticated et permissions desactivees

--- authentication/admin.py
+++ authentication/admin.py
@@ -5,7 +5,8 @@ from .models import CustomUser  # Assuming your model is in the same app
 
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = ['id', 'username', 'can_be_contacted',
-                    'can_data_be_shared', 'created_time']  # Fields to display in the list view
+                    'can_data_be_shared', 'created_time', 'is_superuser', 'is_active',
+                    'is_staff']  # Fields to display in the list view
     search_fields = ['username', 'email']  # Fields to search by
     list_filter = ['can_be_contacted', 'can_data_be_shared', 'groups']  # Fields to filter by
     ordering = ['username', ]  # Default sorting by username
--- authentication/permissions.py
+++ authentication/permissions.py
@@ -11,11 +11,10 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         # Read permissions are allowed to any request,
         # so we'll always allow GET, HEAD or OPTIONS requests.
-        if request.method in permissions.SAFE_METHODS:
-            return True
+
 
         # Write permissions are only allowed to the owner of the snippet.
-        return obj == request.user
+        return request.user.is_superuser or obj == request.user
 
 
 # definir permissionsDenied
@@ -27,6 +26,16 @@ class IsAdminAuthenticated(BasePermission):
         return bool(request.user and request.user.is_authenticated and request.user.is_superuser)
 
 
-class IsCreationOrIsAuthenticated(BasePermission):
+class IsCreationAndIsStaff(BasePermission):
     def has_permission(self, request, view):
-        return view.action == "create" or request.user.is_authenticated
+        if request.method == "GET":
+            return request.user and request.user.is_staff
+        return True
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.user.is_staff:
+            return True
+        # Write permissions are only allowed to the owner of the snippet.
+        return obj == request.user
